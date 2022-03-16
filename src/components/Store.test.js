5435b8249b2a93e4b2aba4e72b035a0a5c3c6705
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-03-16 20:50:10 -0300

    add tests to Store component

---
+++ src/components/Store.test.js
@@ -0,0 +1,52 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Store from "./Store";
+
+describe("<Store />", () => {
+  beforeEach(()=>{
+    render(<Store />);
+  })
+  const product = { id: 1, name: "Stormtrooper T-shirt", description: "White t-shirt, black threads" };
+  describe("when Store is rendered", () => {
+    it("renders the data of one product", () => {
+      const productName = screen.getByText(product.name);
+      const productDescription = screen.getByText(product.description);
+      expect(productName).toBeInTheDocument();
+      expect(productDescription).toBeInTheDocument();
+    });
+
+    it("shows Cart button", () => {
+      const button = screen.getByRole("button", { name: "Cart" });
+      userEvent.click(button);
+      expect(button).toBeInTheDocument();
+    });
+
+    it("shows Products button", () => {
+      const button = screen.getByRole("button", { name: "Products" });
+      userEvent.click(button);
+      expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("when a product is being added to cart", () => {
+    it("shows a product to cart", () => {
+      const addToCartButton = screen.getByTestId("1")
+      userEvent.click(addToCartButton);
+      const cartButton = screen.getByRole("button", { name: "Cart" });
+      userEvent.click(cartButton);
+      expect(screen.getByText(/stormtrooper t\-shirt/i)).toBeInTheDocument();
+    });
+  });
+
+  describe("when a product is being removed from cart", () => {
+    it("removes product from cart", () => {
+      const addToCartButton = screen.getByTestId("1")
+      userEvent.click(addToCartButton);
+      const cartButton = screen.getByRole("button", { name: "Cart" });
+      userEvent.click(cartButton);
+      const removeFromCartButton = screen.getByRole("button", { name: "Remove From Cart" });
+      userEvent.click(removeFromCartButton);
+      expect(screen.queryByText(/stormtrooper t\-shirt/i)).not.toBeInTheDocument();
+    });
+  });
+});
