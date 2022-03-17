d325a1096fa0d8ccbfbe5faae761860c2bc070e7
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-03-17 19:27:06 -0300

    update style

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 const Product = ({product, addToCart}) => {
   return (   
   <div className="product">
-    <img className="product__img"src={product.image} alt={product.name} />
+    <div className="product__img" style={{backgroundImage: `url(${product.image})`}}></div>
     <p className="product__name">{product.name}</p>
     <p className="product__description">{product.description}</p>
     <button onClick={addToCart} className="button button--add" data-testid={product.id}>Add to Cart</button>
