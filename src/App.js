5546975d69f57c2797aba274da158849c9dd012a
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-03-29 14:07:45 -0300

    apply suggestion

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Store from './components/Store';
 import theme from './theme';
 import styled from 'styled-components';
 import css from '@styled-system/css'
+import { colors } from '@mui/material';
 
 const Button = styled.button`
   background-color: ${({theme}) => (theme.colors.purple)};
@@ -13,7 +14,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Store/>
-      <Button css={css({backgroundColor: theme.colors.darkBlue})}>Test Button1</Button>
+      <Button css={css({backgroundColor:'darkBlue'})}>Test Button1</Button>
       <Button>Test Button2</Button>
     </ThemeProvider>
   );
