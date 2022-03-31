9170541e3cd4e049259110374a86a80722b26b08
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-03-31 17:28:29 -0300

    refactor payments texts

--- src/components/Checkout/PaymentForm.js
+++ src/components/Checkout/PaymentForm.js
@@ -1,69 +1,56 @@
 import * as React from 'react';
-import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import { Text } from '../Text';
+import { Input } from '../Input';
 
 export default function PaymentForm({paymentForm, handlePaymentForm}) {
   return (
     <React.Fragment>
-      <Typography variant="h6" gutterBottom>
+      <Text level={3}>
         Payment method
-      </Typography>
+      </Text>
       <Grid container spacing={3}>
         <Grid item xs={12} md={6}>
-          <TextField
+          <Text level={4} color="darkGrey">Name on card</Text>
+          <Input
             required
             id="cardName"
             name="cardName"
-            label="Name on card"
-            fullWidth
-            autoComplete="cc-name"
-            variant="standard"
             value={paymentForm.cardName}
             onChange={handlePaymentForm}
           />
         </Grid>
         <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardNumber"
-            name="cardNumber"
-            label="Card number"
-            fullWidth
-            autoComplete="cc-number"
-            variant="standard"
-            value={paymentForm.cardNumber}
-            onChange={handlePaymentForm}
-          />
+          <Text level={4} color="darkGrey">Card Number</Text>
+            <Input
+              required
+              id="cardNumber"
+              name="cardNumber"
+              value={paymentForm.cardNumber}
+              onChange={handlePaymentForm}
+            />
         </Grid>
         <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="expDate"
-            label="Expiry date"
-            name="expiryDate"
-            fullWidth
-            autoComplete="cc-exp"
-            variant="standard"
-            value={paymentForm.expiryDate}
-            onChange={handlePaymentForm}
-          />
+          <Text level={4} color="darkGrey">Expiry date</Text>
+            <Input
+              required
+              id="expDate"
+              name="expiryDate"
+              value={paymentForm.expiryDate}
+              onChange={handlePaymentForm}
+            />
         </Grid>
         <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            name="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-            autoComplete="cc-csc"
-            variant="standard"
-            value={paymentForm.cvv}
-            onChange={handlePaymentForm}
-          />
+          <Text level={4} color="darkGrey">CVV</Text>
+            <Input
+              required
+              id="cvv"
+              name="cvv"
+              value={paymentForm.cvv}
+              onChange={handlePaymentForm}
+            />
         </Grid>
         <Grid item xs={12}>
           <FormControlLabel
